79502e228e515f668d149e03e1b89a1dcac82bc4
arianaishmo <60683323+arianaishmo@users.noreply.github.com>
2021-05-22 21:52:24 -0700

    Escena change

---
+++ Node.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export (PackedScene) var sce_game
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass 
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Escena_pruebas.tscn")
